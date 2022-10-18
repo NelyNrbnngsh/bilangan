c57eae662144e15da2de282fd0febb5caf34fa93
NelyNrbnngsh <113583375+NelyNrbnngsh@users.noreply.github.com>
2022-10-19 00:22:15 +0700

    Add files via upload

---
+++ irrasional/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BilanganIrrasionalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'irrasional'
---
+++ irrasional/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def BilanganIrrasional(request):   
+    return render(request, 'irrasional.html')
